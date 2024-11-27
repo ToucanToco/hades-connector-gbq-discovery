7e446788703a22c003146a61b33beba58a7f9427
Fanaen <contact@fanaen.fr>
2024-11-27 16:12:57 +0100

    feat(gcp-bigquery-client, google-cloud-bigquery): get dataset list

--- gcp-bigquery-client/src/main.rs
+++ gcp-bigquery-client/src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use gcp_bigquery_client::dataset::ListOptions;
+
+
+const PROJECT_ID: &str = "biquery-integration-tests";
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    // Init BigQuery client
+    let client = gcp_bigquery_client::Client::from_service_account_key_file("../auth/biquery-integration-tests-70c0f9dad952.json").await?;
+    
+    let result = client.dataset().list(PROJECT_ID, ListOptions::default()).await?;
+
+    println!("{result:#?}");
+
+    // Query
+    // let mut query_response = client
+    //     .job()
+    //     .query(
+    //         project_id,
+    //         QueryRequest::new(format!(
+    //             "SELECT COUNT(*) AS c FROM `{}.{}.{}`",
+    //             project_id, dataset_id, table_id
+    //         )),
+    //     )
+    //     .await?;
+    // let mut rs = ResultSet::new_from_query_response(query_response);
+    // while rs.next_row() {
+    //     println!("Number of rows inserted: {}", rs.get_i64_by_name("c")?.unwrap());
+    // }
+
+    Ok(())
 }
--- google-cloud-bigquery/src/main.rs
+++ google-cloud-bigquery/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use google_cloud_bigquery::client::{google_cloud_auth::credentials::{CredentialsFile}, Client, ClientConfig};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let json = include_str!("../../auth/biquery-integration-tests-70c0f9dad952.json");
+    let credentials = CredentialsFile::new_from_str(&json).await?;
+
+    let (config, project_id) = ClientConfig::new_with_credentials(credentials).await.unwrap();
+    let client = Client::new(config).await.unwrap();
+
+    let project_id = project_id.expect("missing project_id");
+
+    let result = client.dataset().list(&project_id, None).await?;
+
+    println!("{result:#?}");
+
+    Ok(())
 }
