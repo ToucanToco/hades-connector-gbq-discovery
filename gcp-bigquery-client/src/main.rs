810d8f5636e36ef433de2c64dd24bb88eff3f2b0
Fanaen <contact@fanaen.fr>
2024-11-28 16:21:58 +0100

    feat(gcp-bigquery-client, google-cloud-bigquery): select example

--- gcp-bigquery-client/src/main.rs
+++ gcp-bigquery-client/src/main.rs
@@ -1,32 +1,46 @@
-use gcp_bigquery_client::dataset::ListOptions;
-
+use gcp_bigquery_client::model::{
+    query_request::QueryRequest, query_response::QueryResponse,
+    table_field_schema::TableFieldSchema,
+};
 
 const PROJECT_ID: &str = "biquery-integration-tests";
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     // Init BigQuery client
-    let client = gcp_bigquery_client::Client::from_service_account_key_file("../auth/biquery-integration-tests-70c0f9dad952.json").await?;
-    
-    let result = client.dataset().list(PROJECT_ID, ListOptions::default()).await?;
+    let client = gcp_bigquery_client::Client::from_service_account_key_file(
+        "../auth/biquery-integration-tests-70c0f9dad952.json",
+    )
+    .await?;
 
-    println!("{result:#?}");
+    let QueryResponse { schema, rows, .. } = client
+        .job()
+        .query(
+            PROJECT_ID,
+            QueryRequest {
+                query: "SELECT * FROM beers.beers_tiny".to_string(),
+                ..Default::default()
+            },
+        )
+        .await?;
 
-    // Query
-    // let mut query_response = client
-    //     .job()
-    //     .query(
-    //         project_id,
-    //         QueryRequest::new(format!(
-    //             "SELECT COUNT(*) AS c FROM `{}.{}.{}`",
-    //             project_id, dataset_id, table_id
-    //         )),
-    //     )
-    //     .await?;
-    // let mut rs = ResultSet::new_from_query_response(query_response);
-    // while rs.next_row() {
-    //     println!("Number of rows inserted: {}", rs.get_i64_by_name("c")?.unwrap());
-    // }
+    let fields: Vec<TableFieldSchema> = schema
+        .map(|s| s.fields)
+        .into_iter()
+        .flatten()
+        .flatten()
+        .collect();
+
+    for field in fields {
+        println!("COL {:?}: {:?}", field.name, field.r#type);
+    }
+
+    for row in rows.iter().flatten() {
+        println!("ROW");
+        for cell in row.columns.iter().flatten() {
+            println!("{:?}", cell.value);
+        }
+    }
 
     Ok(())
 }
--- google-cloud-bigquery/src/main.rs
+++ google-cloud-bigquery/src/main.rs
@@ -1,18 +1,72 @@
-use google_cloud_bigquery::client::{google_cloud_auth::credentials::{CredentialsFile}, Client, ClientConfig};
+use google_cloud_bigquery::{
+    client::{google_cloud_auth::credentials::CredentialsFile, Client, ClientConfig},
+    http::{
+        job::query::{QueryRequest, QueryResponse},
+        table::list::ListTablesRequest,
+    },
+};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let json = include_str!("../../auth/biquery-integration-tests-70c0f9dad952.json");
     let credentials = CredentialsFile::new_from_str(&json).await?;
 
-    let (config, project_id) = ClientConfig::new_with_credentials(credentials).await.unwrap();
+    let (config, project_id) = ClientConfig::new_with_credentials(credentials)
+        .await
+        .unwrap();
     let client = Client::new(config).await.unwrap();
 
     let project_id = project_id.expect("missing project_id");
 
-    let result = client.dataset().list(&project_id, None).await?;
+    let QueryResponse { rows, schema, .. } = client
+        .job()
+        .query(
+            &project_id,
+            &QueryRequest {
+                query: "SELECT * FROM beers.beers_tiny".to_string(),
+                ..Default::default()
+            },
+        )
+        .await?;
 
-    println!("{result:#?}");
+    for field in schema.iter().flat_map(|s| &s.fields) {
+        println!("COL {:?}: {:?}", field.name, field.data_type);
+    }
+
+    for row in rows.iter().flatten() {
+        println!("ROW");
+        for cell in &row.f {
+            println!("{:?}", cell.v);
+        }
+    }
+
+    Ok(())
+}
+
+async fn _notes() -> anyhow::Result<()> {
+    let json = include_str!("../../auth/biquery-integration-tests-70c0f9dad952.json");
+    let credentials = CredentialsFile::new_from_str(&json).await?;
+
+    let (config, project_id) = ClientConfig::new_with_credentials(credentials)
+        .await
+        .unwrap();
+    let client = Client::new(config).await.unwrap();
+
+    let project_id = project_id.expect("missing project_id");
+
+    // Fails when there is no table in result
+    {
+        println!(
+            "{:#?}",
+            client
+                .table()
+                .list(&project_id, "tpch_0_1", &ListTablesRequest::default())
+                .await?
+        );
+        // Error: error decoding response body
+        // Caused by:
+        // missing field `tables` at line 5 column 1
+    }
 
     Ok(())
 }
